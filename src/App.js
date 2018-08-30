73a4c2ea917599d56ff24e614300c91a9d9e312a
zzy1661 <1556597051@qq.com>
2018-08-30 14:11:15 +0800

    add app根路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";	
 import './App.css';
 import Basic from './examples/Basic';
 class App extends Component {
   render() {
     return (
+    	
       <div className="App">
-        <Basic></Basic>
+	      <Router>
+				    <div>
+				      <ul>
+				        <li>
+				          <Link to="/basic">basic</Link>
+				        </li>
+				        
+				      </ul>
+				
+				      <hr />
+				      
+							<Route exact path="/" render={ ()=> ( <Redirect to="/basic" />)} />
+				      <Route exact path="/basic" component={Basic} />
+				      
+				    </div>
+				  </Router>
+        
       </div>
     );
   }
