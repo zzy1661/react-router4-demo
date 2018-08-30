a39a3d948c0be637c8a7853e9e4aad85122be354
zzy1661 <1556597051@qq.com>
2018-08-30 23:51:39 +0800

    modify basic 删除router标签

--- src/examples/Basic/index.js
+++ src/examples/Basic/index.js
@@ -6,27 +6,27 @@ import Topics from './Topics';
 class BasicExample extends Component{ 
 	render()  {
 		return (
-			<Router>
-			    <div>
-			      <ul>
-			        <li>
-			          <Link to={`${this.props.match.url}`}>Home</Link>
-			        </li>
-			        <li>
-			          <Link to={`${this.props.match.url}/about`}>About</Link>
-			        </li>
-			        <li>
-			          <Link to={`${this.props.match.url}/topics`}>Topics</Link>
-			        </li>
-			      </ul>
-			
-			      <hr />
-			
-			      <Route exact path={`${this.props.match.url}`} component={Home} />
-			      <Route path={`${this.props.match.url}/about`} component={About} />
-			      <Route path={`${this.props.match.url}/topics`} component={Topics} />
-			    </div>
-			  </Router>
+	
+				<div>
+					<ul>
+						<li>
+							<Link to={`${this.props.match.url}`}>Home</Link>
+						</li>
+						<li>
+							<Link to={`${this.props.match.url}/about`}>About</Link>
+						</li>
+						<li>
+							<Link to={`${this.props.match.url}/topics`}>Topics</Link>
+						</li>
+					</ul>
+		
+					<hr />
+		
+					<Route exact path={`${this.props.match.url}`} component={Home} />
+					<Route path={`${this.props.match.url}/about`} component={About} />
+					<Route path={`${this.props.match.url}/topics`} component={Topics} />
+				</div>
+		
 		)
 	  
 	}
